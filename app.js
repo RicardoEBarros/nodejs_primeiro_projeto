7c13ce189467c37ac5e5ef2bcfe1fe6d8f923c76
Ricardo Barros <169519@upf.br>
2022-09-14 18:00:26 -0300

    feat: created products route types post and get

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const express = require('express')
+const { randomUUID } = require('crypto')
 
-const app = express()
+const app      = express()
+const products = []
 
-app.get('/primeira-rota', (request, response) => {
-    return response.json({ message: 'Acessou a primeira rota'})
+/* define a middleware */
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+/* post request */
+app.post('/products', (request, response) => {
+    
+    const { name, price } = request.body
+    
+    const product = {
+        id: randomUUID(),
+        name,
+        price
+    }
+
+    products.push(product)
+
 })
+
+app.get('/products', (request, response) => {
+    return response.json(products)
+})
+
+
 
 app.listen(4002, () => console.log('Servidor está rodando na porta 4002'))
